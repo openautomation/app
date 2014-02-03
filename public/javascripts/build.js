d1350450c544df561cd1bffd4dab2ff7fcc12c85
Lance Pollard <lancejpollard@gmail.com>
2014-02-02 22:31:43 -0800

    asdf

--- public/javascripts/build.js
+++ public/javascripts/build.js
@@ -9754,7 +9754,9 @@ function sendMove(remote) {
   // resource('action').create(remote, function(){
   //   console.log('done', arguments);
   // });
-  console.log('selected', JSON.stringify(remote));
+  document.querySelector('#log').appendChild(
+    document.createTextNode('selected ' + JSON.stringify(remote))
+  );
 
   agent.post('/actions')
     .send({ type: 'move', position: remote })
