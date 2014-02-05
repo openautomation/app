79da87e3474bc4dd8eabe6bfe31aa8750562c317
Administrator <administrator@autobox.(none)>
2014-02-04 16:27:54 -0800

    hooked up node-grbl

--- routes/action.js
+++ routes/action.js
@@ -3,7 +3,10 @@
  * Module dependencies.
  */
 
-var move = require('../lib/xyz_sender');
+//var move = require('../lib/grbl-gantry/gantry');
+var grbl = require('grbl');
+var fs = require('fs');
+
 
 /**
  * Execute an action.
@@ -13,7 +16,28 @@ exports.create = function(req, res){
   var action = req.body;
 
   console.log(action);
-  move(10, 10)
+
   // XXX: should call ROS script
-  res.json(action);
+  x = "x10";
+  y = "y0";
+  z = "z0";
+  grbl(function(machine) {
+	cmd = ["G1", x, y, z, "f10000", "\n"].join(" ");
+	console.log(cmd);
+	machine.write(cmd);
+  });
 };
+
+
+grbl(function(machine) {
+        console.log("configuring grbl...");
+
+        machine.write("$X\n");
+        machine.write("G91\n");
+	//machine.write("G92 x0 y0 z0\n");
+
+        machine.write("", function() {
+            console.log("done!");
+            machine.end();
+	});
+});
