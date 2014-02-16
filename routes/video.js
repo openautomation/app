245d9b51d80c15eea9be3d9019a9c6e5104b97fe
Lance Pollard <lancejpollard@gmail.com>
2014-02-16 12:51:38 -0800

    try to pipe to output

--- routes/video.js
+++ routes/video.js
@@ -21,28 +21,28 @@ exports.stream = function *(next){
   // ffmpeg.stdout.on('data', function (data) {
   //   //console.log('ffmpeg stdout: ' + data);
   // });
-
-  var ffmpeg = spawn("ffmpeg", [
-      "-f", "video4linux2",
-      "-i", "/dev/video0",
-      "-re",                   // Real time mode
-      "-f","x11grab",          // Grab screen
-      "-r","100",              // Framerate
-      "-vf", "scale=1024:768",
-      "-s","640x480",   // Capture size
-      "-g","0",                // All frames are i-frames
-      "-me_method","zero",     // Motion algorithms off
-      "-flags2","fast",
-      "-vcodec","libvpx",      // vp8 encoding
-      "-preset","ultrafast",
-      "-tune","zerolatency",
-      "-b:v","1M",             // Target bit rate
-      "-crf","40",             // Quality
-      "-qmin","5",             // Quantization
-      "-qmax","5",
-      "-f","mp4",             // File format
-      "-"                      // Output to STDOUT
-  ]);
+  var opts = '-s 640x480 -f video4linux2 -i /dev/video0 -f mp4 -b 800k -r 30 -vf scale=1024:768 -'.split(' ');
+  var ffmpeg = spawn("ffmpeg", opts
+      // "-f", "video4linux2",
+      // "-i", "/dev/video0",
+      // "-re",                   // Real time mode
+      // "-f","x11grab",          // Grab screen
+      // "-r","100",              // Framerate
+      // "-vf", "scale=1024:768",
+      // "-s","640x480",   // Capture size
+      // "-g","0",                // All frames are i-frames
+      // "-me_method","zero",     // Motion algorithms off
+      // "-flags2","fast",
+      // "-vcodec","libvpx",      // vp8 encoding
+      // "-preset","ultrafast",
+      // "-tune","zerolatency",
+      // "-b:v","1M",             // Target bit rate
+      // "-crf","40",             // Quality
+      // "-qmin","5",             // Quantization
+      // "-qmax","5",
+      // "-f","mp4",             // File format
+      // "-"                      // Output to STDOUT
+  );
   // Pipe the video output to the client response
   //this.body = ffmpeg.stdout;
   var stream = this.body = new PassThrough();
