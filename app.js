1d7d9e94a4b58c8487f08c71a7692966616a400a
Lance Pollard <lancejpollard@gmail.com>
2014-02-16 11:50:43 -0800

    stuff

--- app.js
+++ app.js
@@ -43,17 +43,17 @@ app.use(route.post('/run', actions.create));
 require('./server/video-stream-server.js');
 
 //start video streaming to local websocket
-ffmpeg_args = '-s 640x480 -f video4linux2 -i /dev/video0 -f mpeg1video -b 800k -r 30 -vf scale=1024:768 http://localhost:8082/password/1024/768/';
-ffmpeg = spawn('ffmpeg', ffmpeg_args.split(' '));
+var ffmpeg_args = '-s 640x480 -f video4linux2 -i /dev/video0 -f mpeg1video -b 800k -r 30 -vf scale=1024:768 http://localhost:8082/password/1024/768/';
+var ffmpeg = spawn('ffmpeg', ffmpeg_args.split(' '));
 ffmpeg.on('close', function (code) {
   console.log('ffmpeg video streaming process exited with code ' + code);
 });
-/*ffmpeg.stderr.on('data', function (data) {
+ffmpeg.stderr.on('data', function (data) {
   console.log('ffmpeg stderr: ' + data);
 });
-/*ffmpeg.stdout.on('data', function (data) {
+ffmpeg.stdout.on('data', function (data) {
   console.log('ffmpeg stdout: ' + data);
-});*/
+});
 
 
 /**
